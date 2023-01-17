55ddb50517d2268deb54fa07d3731646b529e0a6
Rajesh kumar <rajeshbabu656656@gmail.com>
2023-01-17 21:16:48 +0530

    Adding numbers to Stack

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class LinkedList
+    {
+
+        public Node head;
+
+        public LinkedList()
+        {
+            this.head = null;
+        }
+
+        public void Push(int data)
+        {
+            Node objNode = new Node(data);
+            if (head == null)
+            {
+                objNode.next = null;
+            }
+            else
+            {
+                objNode.next = head;
+            }
+            head = objNode;
+            Console.WriteLine($"{data} pushed to Stack");
+        }
+
+        public void Display()
+        {
+            int size = 0;
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nStack is empty");
+                return;
+            }
+            Console.Write("\nLinked List => ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+                size++;
+            }
+            Console.WriteLine("\nTotal Elements in Stack: " + size);
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Problem");
+
+            LinkedList objList = new LinkedList();
+            objList.Push(56);
+            objList.Push(30);
+            objList.Push(70);
+            objList.Display();
+        }
+    }
+}
